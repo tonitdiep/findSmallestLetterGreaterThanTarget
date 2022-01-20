69737833119268872f209cc227a0ffa703a80773
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-01-20 23:28:54 +0000

    question problem

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+
+};
+
+/*
+letters[i] is lowercase English letter
+letters is osrted non-decreasing order
+letters contains at least 2 different characters
+target is a lowercase English letter
+
+Question: Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target
+
+Note that the letters wrap around.
+For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.
+ 
+
+Example 1:
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+*/
+
